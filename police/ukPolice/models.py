8bffad366459d9fde72e123ce3d82d42999404c1
Alexandru Martinas <alexandrumartinas33@gmail.com>
2017-04-14 12:09:13 +0200

    Minor fixes

--- police/ukPolice/models.py
+++ police/ukPolice/models.py
@@ -7,14 +7,14 @@ from datetime import date
 
 class Crime(models.Model):
     category = models.TextField(max_length=20)
-    persisten_id = models.TextField(max_length=20, blank=True, null=True)
-    latitude = models.IntegerField(blank=True, null=True)
-    longitude = models.IntegerField(blank=True, null=True)
+    persisten_id = models.TextField(max_length=60, blank=True, null=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
     date = models.DateField(default=date.today)
     policeman_assignated = models.ForeignKey(User, default='')
 
     def __unicode__(self):
-        return self.category+"."+self.persisten_id
+        return str(self.category)+"."+str(self.date)
 
 
 class Outcome(models.Model):
@@ -23,35 +23,35 @@ class Outcome(models.Model):
     Associated_crime = models.ForeignKey(Crime)
 
     def __unicode__(self):
-        return self.Associated_crime.persisten_id+"."+self.name
+        return self.name+"."+str(self.code)
 
 class StreetLevelCrime(models.Model):
     crime = models.ForeignKey(Crime)
-    street_id = models.TextField(max_length=20)
-    street_name = models.TextField(max_length=20)
+    street_id = models.TextField(max_length=50)
+    street_name = models.TextField(max_length=50)
 
     def __unicode__(self):
         return str(self.crime)+"."+str(self.street_name)
 
 class StreetLevelOutcome(models.Model):
     outcome = models.ForeignKey(Outcome)
-    street_id = models.TextField(max_length=20)
-    street_name = models.TextField(max_length=20)
+    street_id = models.TextField(max_length=50)
+    street_name = models.TextField(max_length=50)
 
     def __unicode__(self):
         return str(self.outcome)+"."+str(self.street_name)
 
 class Neighbourhood(models.Model):
-    n_id = models.TextField(max_length=20)
-    n_name = models.TextField(max_length=30)
+    n_id = models.TextField(max_length=40)
+    n_name = models.TextField(max_length=50)
 
     def __unicode__(self):
         return self.n_id+"."+self.n_name
 
 class NeighbourhoodPriority(models.Model):
-    action = models.TextField(max_length=20,blank=True,null=True)
+    action = models.TextField(max_length=400,blank=True,null=True)
     action_date = models.DateField(default=date.today,blank=True,null=True)
-    issue = models.TextField(max_length=50)
+    issue = models.TextField(max_length=400)
     issue_date = models.DateField(default=date.today)
     neighbourhood = models.ForeignKey(Neighbourhood)
 
